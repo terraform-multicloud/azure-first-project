145f12d047851f5f964938cfc0bcbe08625e9bb3
himanshurathi <himanshu.rathi@talktotechie.com>
2026-02-02 15:43:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,3 +9,30 @@ resource "azurerm_resource_group" "rs2" {
     location = "West US"
 }
 
+resource "azurerm_resource_group" "rs1" {
+    name     = "himanshu-resource-group-one"
+    location = var.location
+  
+}
+
+resource "azurerm_resource_group" "rs2" {
+    name     = "himanshu-resource-group-two"
+    location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet1" {
+    name                = var.vnet-name
+  location            = azurerm_resource_group.rs1.location
+  resource_group_name = azurerm_resource_group.rs1.name
+  address_space       = ["10.0.0.0/16"]
+  
+}
+resource "azurerm_subnet" "vnet-sub1" {
+    name                 = "${var.vnet-name}-subnet1"
+  resource_group_name  = azurerm_resource_group.rs1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.0.1.0/24"]
+  
+}
+
+
