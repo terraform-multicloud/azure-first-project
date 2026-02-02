56142f534c593ed212b080bbfde2ca03af673986
himanshurathi <himanshu.rathi@talktotechie.com>
2026-02-02 16:00:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,5 +25,22 @@ resource "azurerm_subnet" "vnet-sub1" {
   
 }
 
+resource "azurerm_storage_account" "st1" {
+    name                     = "himanshustorageaccnt1"
+    resource_group_name      = azurerm_resource_group.rs1.name
+    location                 = azurerm_resource_group.rs1.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+  
+}
+
+resource "azurerm_storage_container" "sc1" {
+    name                  = "himanshucontainerone-bucket1"
+    storage_account_id  = azurerm_storage_account.st1.id
+    container_access_type = "private"
+  
+}
+
+
 
 
