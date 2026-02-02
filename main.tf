acdc3147dbb545cffc5853ef39d1073791221d49
himanshurathi <himanshu.rathi@talktotechie.com>
2026-02-02 15:20:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,9 @@ resource "azurerm_resource_group" "rs1" {
     location = "West US"
   
 }
+
+resource "azurerm_resource_group" "rs2" {
+    name     = "himanshu-resource-group-two"
+    location = "West US"
+}
+
